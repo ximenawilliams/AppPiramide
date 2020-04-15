f9505f9fff1ff36db5449dc82cf30868af24a283
Ximena Williams <ximenawilliams08@yahoo.com>
2020-04-15 04:29:52 -0500

    almuerzo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,34 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'hortaliza',
+    loadChildren: () => import('./hortaliza/hortaliza.module').then( m => m.HortalizaPageModule)
+  },
+  {
+    path: 'frutas',
+    loadChildren: () => import('./frutas/frutas.module').then( m => m.FrutasPageModule)
+  },
+  {
+    path: 'cereales',
+    loadChildren: () => import('./cereales/cereales.module').then( m => m.CerealesPageModule)
+  },
+  {
+    path: 'desayuno',
+    loadChildren: () => import('./desayuno/desayuno.module').then( m => m.DesayunoPageModule)
+  },
+  {
+    path: 'almuerzo',
+    loadChildren: () => import('./almuerzo/almuerzo.module').then( m => m.AlmuerzoPageModule)
+  },
+  {
+    path: 'cena',
+    loadChildren: () => import('./cena/cena.module').then( m => m.CenaPageModule)
+  },
+  {
+    path: 'picadas',
+    loadChildren: () => import('./picadas/picadas.module').then( m => m.PicadasPageModule)
   }
 ];
 @NgModule({
